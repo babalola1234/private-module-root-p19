3d9b76e14378b3d31aaae79f5637a0a7b87899b0
babalola1234 <95980087+babalola1234@users.noreply.github.com>
2023-01-09 11:57:07 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+module "private-s3-webapp-p19" {
+  source  = "app.terraform.io/babadeen-link/private-s3-webapp-p19/aws"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
