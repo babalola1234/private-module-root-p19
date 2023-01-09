8ab3d301a75140b84580a9eea60b089771cdef8f
babalola1234 <95980087+babalola1234@users.noreply.github.com>
2023-01-09 13:14:40 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "website_endpoint" {
   description = "HTTP endpoint for example website."
-  value       = module.s3-webapp.endpoint
+  value       = module.private-s3-webapp-p19.endpoint
 }
